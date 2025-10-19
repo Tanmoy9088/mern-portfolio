0816039bef1f3d98b21bb6457920afc2b75c8d29
Tanmoy9088 <tanmoyss007@gmail.com>
2025-10-19 15:51:45 +0530

    bug fixed

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -141,7 +141,7 @@ function Hero() {
             </span>
 
             {/* Underline effect */}
-            <span className="absolute left-1/2 -translate-x-1/2 bottom-0 w-0 h-[2px] bg-current transition-all duration-300 group-hover:w-full"></span>
+            <span className="absolute left-0 bottom-0 w-0 h-[2px] bg-current transition-all duration-500 group-hover:w-full"></span>
           </div>
         </h1>
 
